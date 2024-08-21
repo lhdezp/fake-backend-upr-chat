af301f41ebdb260937a5fd67907dc3ed019a6506
lhdezp <lhdezp0211@gmail.com>
2024-08-21 01:00:33 -0400

    User authentication bug fix

--- routers/user.py
+++ routers/user.py
@@ -1,20 +1,16 @@
 
-from fastapi import APIRouter, HTTPException, Body
+from fastapi import APIRouter, HTTPException, Body, status
 
 router = APIRouter()
 
+
 @router.post('/')
-async def autenticate(username:str = Body(..., embed=True), password:str = Body(..., embed=True)):
-    try :
-        flag = False
-        print("asdasdad")
-        if username == "adminuser" and password == "adminpass":
-            flag = True
-        if username == "basicuser" and password == "basicpass":
-            flag = True
-        if not flag:
-            raise HTTPException(401, "Authentication wrong")
-        return "Successfully authenticated"
-    except Exception as e:
-        print(e)
-        return HTTPException(500, detail = str(e))
+async def autenticate(username: str = Body(..., embed=True), password: str = Body(..., embed=True)):
+    flag = False
+    if username == "adminuser" and password == "adminpass":
+        flag = True
+    if username == "basicuser" and password == "basicpass":
+        flag = True
+    if not flag:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    return "Successfully authenticated"
